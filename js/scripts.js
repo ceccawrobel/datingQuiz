7bade8a1ba770bba9452551efc3e027a31a34bb1
Vilija Jozaitis and cecca wrobel <vilija+cecca@>
2019-06-05 19:41:20 -0700

    create html, java, and css files for a brief dating quiz with branching output results

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function() {
+  $("#preferences").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+
+    if (age < 18 && gender === "male") {
+      $("#youngMale").show();
+    } else if (age < 18 && gender === "female") {
+      $("#youngFemale").show();
+    } else if (age < 18 && gender === "other") {
+      $("#youngMale").show();
+      $("#youngFemale").show();
+    } else if (age >= 18 && age <=40 && gender === "male") {
+      $("#midMale").show();
+    } else if (age >= 18 && age <=40 && gender === "female") {
+      $("#midFemale").show();
+    } else if (age >= 18 && age <=40 && gender === "other") {
+      $("#midMale").show();
+      $("#midFemale").show();
+    } else if (age > 40 && gender === "male") {
+      $("#oldMale").show();
+    } else if (age > 40 && gender === "female") {
+      $("#oldFemale").show();
+    } else if (age > 40 && gender === "other") {
+      $("#oldMale").show();
+      $("#oldFemale").show();
+    }
+    
+    event.preventDefault();
+  });
+});
